896059aef1acd2049bc5b0218b3efa2c955debf7
Christopher Campbell <christytc10@gmail.com>
2020-08-26 19:11:01 +0100

    strings

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -496,7 +496,7 @@ if __name__ == "__main__":
     doctor_bot.send(get_injury_report)
 
     stonks_bot = Discord(webhook, "Stonks", "https://m.media-amazon.com/images/I/81l-+mFDVzL._SS500_.jpg")
-    stonks_bot.send(send_any_string, get_trending_players)
+    stonks_bot.send(send_any_string, get_trending_players())
     stonks_bot.send(get_trade_leaders, league_id, get_current_week())
 
     schedule.every(1).wednesday.at("18:30").do(announcements.send, draft_reminder)
