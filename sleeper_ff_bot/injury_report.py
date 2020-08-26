82c1f8c6cf3404795a31a9d2e76f0abb9c3a49d0
Christopher Campbell <christytc10@gmail.com>
2020-08-26 08:25:24 +0100

    Send injury report

--- sleeper_ff_bot/injury_report.py
+++ sleeper_ff_bot/injury_report.py
@@ -6,10 +6,12 @@ def get_injury_report(league_id=485057710979543040):
     for roster in League(league_id).get_rosters():
         rostered_players += roster['players']
 
+    report_string = "Injury Report for currently rostered players: \n"
     players = Players().get_all_players()
     for player in players:
         if player in rostered_players:
             player_obj = players.get(player)
             if player_obj["injury_status"] in ['NA', 'DNR', 'Sus', None]:
                 continue
-            return f'{player_obj["full_name"]}: {player_obj["injury_status"]}, {player_obj["injury_body_part"]} since {player_obj["injury_start_date"]}'
+            report_string += f'{player_obj["full_name"]}: {player_obj["injury_status"]}, {player_obj["injury_body_part"]} since {player_obj["injury_start_date"]}\n'
+    return report_string
