64d8c32c6514d756d726d9950ba6cc8db47275b4
Christopher Campbell <christytc10@gmail.com>
2020-08-26 20:13:35 +0100

    logging

--- sleeper_ff_bot/discord.py
+++ sleeper_ff_bot/discord.py
@@ -16,6 +16,7 @@ class Discord(BotInterface):
                           json={"username": self.username, "avatar_url": self.avatar_url, "content": message})
 
     def send_pure_json(self, json):
+        print(f'SENDING JSON: {json}')
         if self.username is None:
             requests.post(self.webhook, json=json)
         else:
