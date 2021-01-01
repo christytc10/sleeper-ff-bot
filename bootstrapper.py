fd57b5c79f0c2450633724e351a5a1d20a96b83b
Christy Campbell <christytc10@gmail.com>
2021-01-01 19:05:51 +0000

    Threading

--- bootstrapper.py
+++ bootstrapper.py
@@ -7,5 +7,8 @@ notifications_thread = threading.Thread(target=notifications_bot.run_notificatio
 notifications_thread.daemon = True  # Daemonize thread
 notifications_thread.start()  # Start the execution
 
+bot2_thread = threading.Thread(target=bot2.start_bot(), args=())
+bot2_thread.daemon = True  # Daemonize thread
+bot2_thread.start()  # Start the execution
+
 chat_bot.start_bot()
-bot2.start_bot()
